228cdf0dfdcb44a2b9b6674057bfd109d8d17f52
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2022-12-29 15:18:39 -0300

    ✨ style: adding gif logo, changing nav shadow and links scale on hover

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ interface HomeProps {}
  
 const Home: FunctionComponent<HomeProps> = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-between bg-black">
+    <div className="outContainer">
       <Nav />
       <main className="rounded-xl flex min-h-max w-10/12 min-w-min h-full text-white border-white border-2 items-center justify-center flex-col">
         <h1 className="">
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,27 +6,24 @@ interface NavProps {}
  
 const Nav: FunctionComponent<NavProps> = () => {
   return (
-    <nav className="text-white flex flex-col h-full w-1/6 max-w-max border-r-green-300 border-r-2 min-w-fit items-center justify-around left-0 shadow-md shadow-green-300">
-      <section className="w-24">
-        <h2 className="flex flex-col items-center justify-around text-green-300 h-10 font-mono font-bold text-center">
-          Eduardo Stephan
-          {/* <span className="text-green-300">Stephan</span> */}
-        </h2>
+    <nav className="text-white flex flex-col h-full w-32 border-r-green-300 border-r-2 min-w-fit items-center justify-around left-0 shadow-lg shadow-green-300">
+      <Link to="/" className="h-28 w-28">
+        <div className="logo"></div>
+      </Link>
+
+      <section className="flex flex-col justify-between items-center h-36 font-medium">
+        <Link to="/projects" className="navLink">Projects</Link>
+        <Link to="/about" className="navLink">About</Link>
+        <Link to="/contact" className="navLink">Contact</Link>
+        <Link to="/" className="navLink">Home</Link>
       </section>
 
-      <section className="flex flex-col justify-between items-center h-36 text-green-100 font-medium">
-        <Link to="/projects" className=" w-11/12 hover:text-green-400 border-b-2 hover:border-b-green-400 border-b-black">Projects</Link>
-        <Link to="/about" className="hover:text-green-400 border-b-2 hover:border-b-green-400 border-b-black">About</Link>
-        <Link to="/contact" className="hover:text-green-400 border-b-2 hover:border-b-green-400 border-b-black">Contact</Link>
-        <Link to="/" className="hover:text-green-400 border-b-2 hover:border-b-green-400 border-b-black">Home</Link>
-      </section>
-
-      <section className="flex w-full items-center justify-center text-green-100">
+      <section className="flex w-full items-center justify-center">
         <a href="https://www.linkedin.com/in/carlos-eduardo-stephan-851b91233/" target="_blank" rel="noreferrer">
-          <GitHub className="m-2 hover:text-green-400" />
+          <GitHub className="m-2 hover:text-green-400 hover:scale-110" />
         </a>
         <a href="https://github.com/StephanCadu" target="_blank" rel="noreferrer">
-          <LinkedIn className="m-2 hover:text-green-400" />
+          <LinkedIn className="m-2 hover:text-green-400 hover:scale-110" />
         </a>
       </section>
     </nav>
