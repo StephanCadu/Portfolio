49133fc924a06d5d28da947aff5cff4a0cc7bfee
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2023-01-05 16:55:30 -0300

    🛠️  fea: creating techs carousel using ref to scroll on click (3 first techs are not appearing, it still need to be fixed)

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,17 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useRef, useState } from "react";
 import Nav from "./Nav";
 import onlinestore from '../images/onlinestore.png'
 
 interface ProjectsProps {}
 
 interface ITech {
-  name: string,
+  name: string
   image: string
 }
 
-interface IProject extends ITech {
+interface IProject {
+  name: string
+  image: string
   description: string
   techs: string[]
   link: string
@@ -26,7 +28,9 @@ const techs: Array<ITech> = [
   { name: 'Node.js', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg' },
   { name: 'React', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' },
   { name: 'Redux', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg' },
-  { name: 'PostgreSQL', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg' }
+  { name: 'PostgreSQL', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg' },
+  { name: 'Mocha', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg' },
+  { name: 'Sequelize', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg' },
 ];
 
 const myProjects: IProject[] = [
@@ -47,49 +51,73 @@ const myProjects: IProject[] = [
 
 const Projects: FunctionComponent<ProjectsProps> = () => {
   const [skills, setSkills] = useState<string[]>([]);
-  // const [projects, setProjects] = useState<IProject[]>([]);
+  const ref = useRef<null | HTMLDivElement>(null);
+
+  const projects: IProject[] = myProjects.filter((project) => !skills.length
+    || skills.some((skill) => project.techs.some((tech) => tech === skill)));
+
+  const scroll = (offset: number): void => {
+    if (ref.current !== null) {
+      ref.current.scrollLeft += offset;
+    }
+  }
+
   
   return (
     <div className="outContainer">
       <Nav />
       
       <main className="mainContainer">
-        <h2 className="mb-3 font-bold text-green-200">Select skills:</h2>
+        {/* title */}
+        <h2 className="mb-3 font-bold text-green-200 text-4xl">Projects</h2>
 
-        <div className="flex w-3/4 h-fit items-center justify-center flex-wrap">
-          { techs.map(({ image, name }) => (
-            <img 
-              onClick={() => setSkills(
-                skills.includes(name) 
-                ? skills.filter((skill) => skill !== name) 
-                : [...skills, name])} 
-              className={ `h-11 w-11 mx-6 my-3 p-1 hover:cursor-pointer ${skills.includes(name) ? 'border-b-2 border-b-green-200' : ''}`} 
-              src={ image } 
-              alt={ name } 
-            />
-          )) }
+        {/* techs carousel */}
+        <div className="flex h-16 items-center justify-center skillsContainer">
+          <button className="arrow-left text-white" onClick={() => scroll(-95)}>{ '<' }</button>
+
+          <div className="flex h-14 items-center justify-center skills" ref={ref}>
+            { 
+              techs.map(({ image, name }, index) => (
+                <img 
+                  onClick={() => setSkills(
+                    skills.includes(name) 
+                    ? skills.filter((skill) => skill !== name) 
+                    : [...skills, name])} 
+                  className={ 
+                    `h-11 w-11 mx-6 my-3 p-1 hover:cursor-pointer 
+                    ${skills.includes(name) ? 'border-b-2 border-b-green-200' : ''}
+                    `
+                    // ${index === active ? 'activeSkill' : ''}
+                  }
+                  // ref={index === active ? ref : null}
+                  src={ image } 
+                  alt={ name } 
+                />
+              ))
+            }
+          </div>
+          
+          <button className="arrow-right text-white" onClick={() => scroll(95)}>{ '>' }</button>
         </div>
 
-        <div className="h-3/4 w-5/6">
+        {/* projects */}
+        <div className=" h-2/3 w-5/6">
           {
-            myProjects
-              .filter((project) => !skills.length 
-                || skills.some((skill) => project.techs.some((tech) => tech === skill)))
-              .map(({ name, image, description, techs, link }) => (
-                <div className="flex w-full h-full items-center justify-around flex-col">
-                  <h1 className="text-center font-bold text-3xl text-green-200 my-3">{ name }</h1>
-                  
-                  <a href={ link } className="h-fit w-4/5 rounded-md mx-3">
-                    <img src={ image } alt={ name } />
-                  </a>
+            projects.map(({ name, image, description, techs, link }) => (
+              <div className="flex w-full h-full items-center justify-around flex-col">
+                <h1 className="text-center font-bold text-2xl text-green-200 my-3">{ name }</h1>
+                
+                <a href={ link } className="h-fit w-3/5 max-w-xs rounded-md mx-3">
+                  <img src={ image } alt={ name } />
+                </a>
 
-                  <p className="text-center p-1 w-5/6">{ description }</p>
-                  
-                  <div className="flex w-11/12 h-3/5 items-center justify-center overflow-x-auto">
-                    { techs.map((tech) => <p className="mx-4 text-green-200">{ tech }</p>) }
-                  </div>
+                <p className="text-center p-1 w-5/6">{ description }</p>
+                
+                <div className="flex w-11/12 h-3/5 items-center justify-center overflow-x-auto">
+                  { techs.map((tech) => <p className="mx-4 text-green-200">{ tech }</p>) }
                 </div>
-              ))
+              </div>
+            ))
           }
         </div>
       </main>
